0a2c533dc7da02b17bc2c3a4a48d41a0146fa7cc
Jose Diaz <diazrodriguez.jr@gmail.com>
2020-02-17 22:55:09 -0500

    Proyecto Finalizado

---
+++ router.js
@@ -0,0 +1,40 @@
+class Router {
+    constructor(routes) {
+        this.routes = routes;
+        this._loadInitialRoute();
+    }
+    _loadInitialRoute(){
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length >1 ? pathNameSplit.slice(1):'';
+        this.loadRoute(...pathSegs);
+    }
+    _matchUrlToRoute(urlSegs) {
+        const matchedRoute = this.routes.find(route =>{
+            const routePathSegs = route.path.split('/').slice(1);
+            if (routePathSegs.length !== urlSegs.length) {
+                return false;
+            }
+            return routePathSegs
+                .every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+        })
+        return matchedRoute;
+    }
+    loadRoute(...urlSegs){
+        debugger;
+        const matchedRoute = this._matchUrlToRoute(urlSegs);        
+        const url = `/${urlSegs.join('/')}`;
+        history.pushState({},'this works', url);
+        const routerOutElm = document.querySelectorAll('[data-router]')[0];
+        routerOutElm.innerHTML = matchedRoute.template;
+    }
+}
+
+
+
+
+
+
+
+
+
+
